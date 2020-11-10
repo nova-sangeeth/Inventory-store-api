8c0fb3fe2d9b56b7033ef7272825d455f4eb489a
nova-sangeeth <novasangeeth@outlook.com>
2020-11-10 20:46:14 +0530

    added new model for products

--- inventory_store/api_store/models.py
+++ inventory_store/api_store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length = 128)
+    year_launched = models.IntegerField()
+    manufacturer = models.CharField(max_length = 128)
+
+    def __str__(self):
+        return self.name
+
+
