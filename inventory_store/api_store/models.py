b961a9b7783b31d41cf9ae49556a42eb20db0d24
nova-sangeeth <novasangeeth@outlook.com>
2020-11-11 22:58:55 +0530

    added methods for distinguishing weather it is fresh of not.

--- inventory_store/api_store/models.py
+++ inventory_store/api_store/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
 
+
 class Product(models.Model):
-    name = models.CharField(max_length = 128)
+    name = models.CharField(max_length=128)
     year_launched = models.IntegerField()
-    manufacturer = models.CharField(max_length = 128)
+    manufacturer = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
@@ -13,7 +14,7 @@ class Product(models.Model):
 
 class Batch(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    units = models.IntegerField() 
+    units = models.IntegerField()
     date_produced = models.DateField()
     expiry_date = models.DateField()
     total = models.IntegerField(blank=True)
@@ -25,26 +26,34 @@ class Batch(models.Model):
         else:
             super(Batch, self).save(*args, **kwargs)
 
+    @property
+    def freshness(self):
+        if (date.today() - self.expiry_date).days > 0:
+            return "expired"
+        elif (self.expiry_date - date.today()).days <= 3:
+            return "expiring today..."
+        else:
+            return "fresh"
+
     def __str__(self):
         return f"{self.id} - {self.product}"
 
-        
+
 class Order(models.Model):
     order_date = models. DateField()
-    batch = models.ForeignKey(Batch, on_delete=models.CASCADE, related_name="batch_in_order")
+    batch = models.ForeignKey(
+        Batch, on_delete=models.CASCADE, related_name="batch_in_order")
     units = models.IntegerField()
-    company = models.CharField(max_length = 128)
+    company = models.CharField(max_length=128)
 
     def save(self, *args, **kwargs):
         if not self.id:
             if self.units < self.batch.total:
                 self.batch.total -= self.units
                 self.batch.save()
-                super(Order,self).save(*args, **kwargs)
+                super(Order, self).save(*args, **kwargs)
             else:
                 return None
 
     def __str__(self):
         return f"{self.order_date} - {self.batch}"
-
-
