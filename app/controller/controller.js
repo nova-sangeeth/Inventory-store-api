7ccb58b9dd8b539f8044197d93a12281473da23d
nova-sangeeth <novasangeeth@outlook.com>
2022-02-02 00:23:22 +0530

    Changes to add new controller

---
+++ app/controller/controller.js
@@ -0,0 +1,65 @@
+const { request } = require("express");
+const db = require("../models");
+const ItemsDB = db.items;
+// used to search strings in the title column of the items table
+const Op = db.Sequelize.Op;
+
+exports.testing = (req, res) => {
+  let message = "this route is now working!!!";
+  res.json({ test: message });
+};
+
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  // ItemsDB.findOne({
+  //   where: {
+  //     id: id,
+  //   },
+  // })
+  //   .then((item) => {
+  //     res.json(item);
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).json(err);
+  //   });
+
+  // check if the query param for title exists and then query accordingly.
+  // const query_title = req.query.title;
+  // if (query_title) {
+  //   console.log("Look here == === === === " + query_title);
+  //   ItemsDB.findOne({
+  //     where: {
+  //       title: query_title,
+  //     },
+  //   }).then((item) => {
+  //     res.json(item);
+  //   }).catch ((err) => {
+  //     res.status(500).json(err);
+  //   }
+  //   )
+  // }
+
+  ItemsDB.findByPk(id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
+
+exports.find_title = (req, res) => {
+  const id = req.params.id;
+
+  ItemsDB.findAll({
+    where: {
+      id: id,
+    },
+    attributes: ["title"],
+  })
+    .then((item) => {
+      res.send(item);
+    })
+    .catch((err) => res.status(500).send(err));
+};
